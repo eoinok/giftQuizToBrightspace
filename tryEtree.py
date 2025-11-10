5621cb1bcd1c6b07d2b7f3211589b9bae45f5aeb
Eoin O'Kennedy <eoin.okennedy@tudublin.ie>
2025-11-10 21:09:10 +0000

    added file dialog

--- tryEtree.py
+++ tryEtree.py
@@ -1,33 +1,59 @@
+import tkinter as tk
+from tkinter import filedialog
 from lxml import etree
 
-with open('sample.xml', 'rt') as f:
+# Hide the main tkinter window
+root = tk.Tk()
+root.withdraw()
+
+# Ask user to select an XML file
+xml_file = filedialog.askopenfilename(
+    title="Select Moodle XML file",
+    filetypes=[("XML Files", "*.xml"), ("All Files", "*.*")]
+)
+
+if not xml_file:
+    print("No file selected, exiting.")
+    exit()
+
+# Ask where to save the CSV
+csv_file = filedialog.asksaveasfilename(
+    title="Save CSV as",
+    defaultextension=".csv",
+    filetypes=[("CSV Files", "*.csv"), ("All Files", "*.*")]
+)
+
+if not csv_file:
+    print("No save location selected, exiting.")
+    exit()
+
+# Parse the XML file
+with open(xml_file, 'rt', encoding='utf-8') as f:
     tree = etree.parse(f)
 
-fw = open("sample.csv", "w")
-    
-    
-for question in tree.iter('question'):
-    fw.write("NewQuestion,MC" + "\n")
-    
-    answers = question.findall('answer')
-    for node in question.getiterator():
-        if node.tag=='name':
-            for subnode in node.getiterator():
-                if subnode.tag=='text':
-                    fw.write("Title,")
-                    fw.write(subnode.text + "\n")
-        elif node.tag=='questiontext':
-            for subnode in node.getiterator():
-                if subnode.tag=='text':
-                    fw.write("QuestionText,")
-                    fw.write('"' + subnode.text + '"' + "\n")
-    for answer in answers:
-        attribs = answer.attrib
-        answerweight = attribs['fraction']
-        answertext = answer.find("text")
-        fw.write("Option,")
-        fw.write(answerweight)
-        fw.write(",")
-        fw.write('"' + answertext.text + '"' + "\n")
-        
-fw.close()
+with open(csv_file, "w", encoding='utf-8') as fw:
+    for question in tree.iter('question'):
+        fw.write("NewQuestion,MC\n")
+
+        answers = question.findall('answer')
+        for node in question.iter():
+            if node.tag == 'name':
+                for subnode in node.iter():
+                    if subnode.tag == 'text':
+                        fw.write("Title,")
+                        fw.write(subnode.text or "")
+                        fw.write("\n")
+            elif node.tag == 'questiontext':
+                for subnode in node.iter():
+                    if subnode.tag == 'text':
+                        fw.write("QuestionText,")
+                        text = subnode.text or ""
+                        fw.write(f'"{text}"\n')
+
+        for answer in answers:
+            answerweight = answer.attrib.get('fraction', '')
+            answertext = answer.find('text')
+            text = answertext.text if answertext is not None else ""
+            fw.write(f"Option,{answerweight},\"{text}\"\n")
+
+print(f"✅ Conversion complete!\nSaved to: {csv_file}")
